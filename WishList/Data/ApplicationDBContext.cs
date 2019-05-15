4aef4893994aa4bee12107efbe8d5a06d70e97f5
Theodoros Margomenos <xndingo@outlook.com>
2019-05-15 12:24:59 +0200

    bah

--- WishList/Data/ApplicationDBContext.cs
+++ WishList/Data/ApplicationDBContext.cs
@@ -5,11 +5,10 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Item> Items;
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-
         }
-
-        public DbSet<Item> Items;
     }
 }
--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -5,7 +5,8 @@ namespace WishList.Models
     public class Item
     {
         public int Id { get; set; }
-        [Required, MaxLength(50)]
+        [Required]
+        [MaxLength(50)]
         public string Description { get; set; }
     }
 }
